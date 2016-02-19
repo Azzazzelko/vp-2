abfa0da9fa9339581c8fa8db184649241f433d76
Azzazzelko <azzazzelko@gmail.com>
2016-02-19 09:50:52 +0300

    Реализованы переключатели 3х видов

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
  var lotusMod = (function(){
   var min_max = $("#price-min, #price-max"),
-      boxes = $(".colors__item");
+      boxes = $(".colors__item"),
+      viewButtons = $(".control__view-item_inline-block, .control__view-item_inline, .control__view-item_block"),
+      temp;
 
   var main = function(){
     setUpListeners();
@@ -19,6 +21,8 @@
     boxes.on("click", checkColor);
 
     $(".categories__toggle").on("click", filterContentToggle);
+
+    viewButtons.on("click", changeView);
   };
 
   function column(){ //== Разделение текста в диве на две колонки
@@ -51,7 +55,7 @@
 
         $temp.remove();
         });
-    };
+  };
 
   function onlyNumber(e){ //== Ввод только цифр. + Разрешен делит, бекспейс и num0-9.
       if( e.keyCode == 46 || e.keyCode == 8 || e.keyCode == 13 ){
@@ -61,7 +65,7 @@
       if (((e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
         e.preventDefault();
       }
-    };
+  };
 
   function slider(){ //== Слайдер в общем-то
       var _min = parseInt( $("#price-min").val() ),
@@ -108,6 +112,44 @@
       .slideToggle();
   };
 
+  function changeView(){
+    viewButtons
+      .removeClass('control__view-item_inline-block_active')
+      .removeClass('control__view-item_inline_active')
+      .removeClass('control__view-item_block_active');
+
+    if ( $(this).hasClass('control__view-item_inline-block') ){
+      $(this).addClass('control__view-item_inline-block_active');
+    } else if ( $(this).hasClass('control__view-item_inline') ){
+      $(this).addClass('control__view-item_inline_active');
+    } else if ( $(this).hasClass('control__view-item_block') ){
+      $(this).addClass('control__view-item_block_active');
+    }
+    
+    
+    var data = $(this).attr("data-view"),
+        over9k = $(".product__list").find("*").add(".product__list");
+
+    over9k.each(function(){
+      var classList = $(this).attr("class").split(/\s+/),
+          forClear = $(this);
+
+      $(classList).each(function(index, el){
+        if ( el.indexOf(temp) > 0 ){
+            forClear.removeClass(el);
+        };
+      });
+
+      var newClassName = $(this).attr("class") + data;
+
+      if ( $(this).attr("class").indexOf(data) == -1 ){
+        $(this).addClass(newClassName);
+      }
+    });
+    
+    temp = data;
+  };
+  
   return {
     run:main
   }
@@ -117,5 +159,55 @@
 $(document).ready(function(){
   lotusMod.run();
 });
+
+
+
+
+
+
+
+// var viewButtons = $(".control__view-item_inline-block, .control__view-item_inline, .control__view-item_block");
+// var temp;
+
+
+// viewButtons.on("click", function(){
+//   viewButtons
+//     .removeClass('control__view-item_inline-block_active')
+//     .removeClass('control__view-item_inline_active')
+//     .removeClass('control__view-item_block_active');
+
+//   if ( $(this).hasClass('control__view-item_inline-block') ){
+//     $(this).addClass('control__view-item_inline-block_active');
+//   } else if ( $(this).hasClass('control__view-item_inline') ){
+//     $(this).addClass('control__view-item_inline_active');
+//   } else if ( $(this).hasClass('control__view-item_block') ){
+//     $(this).addClass('control__view-item_block_active');
+//   }
+  
+  
+//   var data = $(this).attr("data-view"),
+//       over9k = $(".product__list").find("*").add(".product__list");
+
+//   over9k.each(function(){
+//     var classList = $(this).attr("class").split(/\s+/),
+//         forClear = $(this);
+
+//     $(classList).each(function(index, el){
+//       if ( el.indexOf(temp) > 0 ){
+//           forClear.removeClass(el);
+//       };
+
+//     });
+
+//     var newClassName = $(this).attr("class") + data;
+
+//     if ( $(this).attr("class").indexOf(data) == -1 ){
+//       $(this).addClass(newClassName);
+//     }
+//   });
+  
+//   temp = data;
+// });
+
 
 
